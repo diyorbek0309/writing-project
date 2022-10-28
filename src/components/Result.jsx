4960bfadcb6fc690321587f72d22b9919e6c9373
Khujaniyazova <Khujaniyazova00@gmail.com>
2022-10-28 13:25:46 -0700

    initial result

---
+++ src/components/Result.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Result from "../pages/Result";
+import style from "./Result.module.css";
+
+
+return (
+  <div>
+    {/* <div className={style.result_table}>
+      <h2>Natija</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Vaqt</th>
+            <th>Wpm</th>
+            <th>Protsent</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>AM</td>
+            <td>Soni</td>
+            <td>%</td>
+          </tr>
+        </tbody>
+      </table>
+    </div> */}
+  </div>
+);
+
+export default Result;
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,7 +1,30 @@
 import React from "react";
+import style from "../components/Result.module.css";
 
 const Result = () => {
-  return <div>Result</div>;
+  return (
+    <div>
+      <div className={style.result_table}>
+        <h2>Natija</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Vaqt</th>
+              <th>Wpm</th>
+              <th>Protsent</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>AM</td>
+              <td>Soni</td>
+              <td>%</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Result;
