71eb51b775c3a80b9adef99def7a1357436a14f0
Diyorbek Olimov <60889787+diyorbek0309@users.noreply.github.com>
2022-10-29 16:14:19 +0500

    finished result page

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,27 +1,45 @@
-import React from "react";
+import { useTranslation } from "react-i18next";
 import style from "../components/Result.module.css";
 
 const Result = () => {
+  const { t } = useTranslation();
+  const result = JSON.parse(localStorage.getItem("history"));
+
   return (
     <div>
       <div className={style.result_table}>
-        <h2>Natija</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Vaqt</th>
-              <th>Wpm</th>
-              <th>Protsent</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>AM</td>
-              <td>Soni</td>
-              <td>%</td>
-            </tr>
-          </tbody>
-        </table>
+        <h2>{t("result.title")}</h2>
+        {result && result.length > 0 ? (
+          <table>
+            <thead>
+              <tr>
+                <th>№</th>
+                <th>WPM</th>
+                <th>{t("result.percent")}</th>
+                <th>{t("result.time")}</th>
+              </tr>
+            </thead>
+            <tbody>
+              {result.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.result}</td>
+                  <td>{item.percent}%</td>
+                  <td>
+                    {`${new Date(item.date).getHours()}:${new Date(
+                      item.date
+                    ).getMinutes()} 
+                      ${new Date(item.date).getDate()}-${
+                      new Date(item.date).getMonth() + 1
+                    }-${new Date(item.date).getFullYear()}`}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <div>{t("result.empty")}</div>
+        )}
       </div>
     </div>
   );
