f006146144d25d66ceb8ec59b65578e61003bb1d
Sobirov Diyorbek <diyorsobirov976@gmail.com>
2022-10-29 12:37:42 +0500

    help_style

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import Modal from "./components/Modal";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
+import Help from "./pages/Help";
 
 export const routes = [
   { path: "/", element: <Home /> },
   { path: "result", element: <Modal /> },
   { path: "history", element: <Result /> },
   { path: "competition", element: <Modal /> },
-  { path: "help", element: <Modal /> },
+  { path: "help", element: <Help /> },
 ];
