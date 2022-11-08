50b468baf0d947f3a5e52f87ba7ee50c38a63db3
Sobirov Diyorbek <diyorsobirov976@gmail.com>
2022-11-08 23:15:31 +0500

    Hmain_responsive style

--- src/components/HSideright.jsx
+++ src/components/HSideright.jsx
@@ -1,9 +1,16 @@
 import "./anime.css";
+import "./Home.module.css";
 
 const HSideright = ({ count, bool1, isLight }) => {
   return (
     <>
-      <div className={`anime ${!bool1 ? "anime2" : ""} ${!isLight ? "tun" : "kun"}`}>{count}</div>
+      <div
+        className={`anime ${!bool1 ? "anime2" : ""} ${
+          !isLight ? "tun" : "kun"
+        }`}
+      >
+        {count}
+      </div>
     </>
   );
 };
