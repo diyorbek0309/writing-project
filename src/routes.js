e7b25730a4ff7cdf1df43f57b0f58bb5d1792bd5
Dilshod966 <bohodirovdilshod3@gmail.com>
2022-11-11 21:54:40 +0500

    competition

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,12 @@ import Modal from "./components/Modal";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
 import Help from "./pages/Help";
+import Comp from "./pages/competition"
 
 export const routes = [
   { path: "/", element: <Home /> },
   { path: "result", element: <Modal /> },
   { path: "history", element: <Result /> },
-  { path: "competition", element: <Modal /> },
+  { path: "competition", element: <Comp /> },
   { path: "help", element: <Help /> },
 ];
