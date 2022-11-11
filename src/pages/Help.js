153c5fa48df383f99cd7ce00c572c13b4099de52
Sobirov Diyorbek <diyorsobirov976@gmail.com>
2022-11-11 23:19:16 +0500

    finish_help

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -1,68 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import "./Help.css";
+import { helpTextUZ, helpTextEN } from "../text";
 import Img from "./working.png";
 
 function Help() {
+  const { t } = useTranslation();
+
+  // const text = "uz" ? helpTextUZ : helpTextEN;
+  const helpText =
+    localStorage.getItem("lang") === "uz" ? helpTextUZ : helpTextEN;
+
+  // console.log(Math.random(helpTextUZ.split("").length) * 700);
+
   return (
     <div className="help_wrap">
       <div className="img_wrap">
         <img src={Img} />
       </div>
       <div className="text_wrap">
-        <h1>Siz uchun foydali</h1>
-        <p>
-          Eng avvalo klaviaturangizga yaxshilab nazar soling. Biz ingliz (lotin)
-          harflari berilgan klaviaturaga qaramasdan yozishni o’rganamiz. Siz
-          ko’rib turgan lotin harflariga asoslangan tugmalarning joylashuvi
-          familiyasi Qwerty bo’lgan mutaxassis tomonidan joylashtirilgan. U
-          klaviaturaning birinchi qatoriga o’zining familiyasini «qwerty» qilib
-          joylashtirgan, shuning uchun hozir dunyoda eng ko’p ishlatiladigan bu
-          standart joylashuvli klaviatura «Qwerty klaviaturasi» deb ham ataladi.
-          <br />
-          <br />
-          Har qanday klaviaturaning lotincha «F» va «J» harflari joylashgan
-          tugmalarda boshqalariga nisbatan ajralib turadigan, bir oz bo’rtib
-          chiqqan joyi bo’ladi. Buning sababi, ikki qo’lning ko’rsatkich
-          barmoqlari shu tugmalarda turishi kerak va matn teruvchi ko’rsatkich
-          barmoqlarini klaviaturaga qaramasdan, tugmachalarning bo’rtib turgan
-          joyini barmoq uchlari bilan his qilgan holda topib olishi kerak. Shu
-          tarzda, ikki qo’lning ko’rsatkich barmoqlari «F» va «J» harflariga
-          qo’yiladi, qolgan har ikki qo’ldagi uchtadan barmoq ketma-ket
-          joylashgan tugmachalar ustiga qo’yiladi. Klaviaturada qo’l mana bunday
-          holatda turishi kerak:
-          <br />
-          <br />
-          Chap qo’l barmoqlari A S D F harflari yozilgan tugmalarda, o’ng qo’l
-          barmoqlari esa J K L ; tugmalari ustida turishi kerak. O’ng qo’lning
-          bosh barmog’i probel tugmasini bosich uchun ishlatilishi kerak.
-          <br />
-          <br />
-          Quyidagi rasmda qo’llarning qaysi barmoqlari klaviaturadagi qaysi harf
-          tugmasini bosishi kerakligi ko’rsatilgan. E’tibor bering,
-          barmoqlarning ranglari ular bosishi kerak bo’lgan klaviatura tugmalari
-          ranglari bilan o’zaro mos:
-          <br />
-          <br />
-          Endi klaviaturaga qaramasdan yozishni mash qila boshlar ekansiz,
-          Sizdan juda katta sabr talab etiladi. Qanchalik ko’p mashq qilsangiz,
-          shunchalik tez o’rganib olasiz. «Tez» deganda Siz klaviaturaga
-          qaramasdan yozishni bir necha soatda o’rganib olasiz, degan ma’no
-          tushunilmasligi kerak. Hatto bir necha kunda ham o’rganmasligingiz
-          mumkin. Bir necha hafta yoki hatto bir necha oylik shug’ullanishdan
-          keyin klaviaturaga qaramasdan, tezlik bilan matn terishni o’rganib
-          olasiz. Mashqda har qancha qiyin bo’lmasin, ishoning, maqsadingizga
-          erishganingizdan so’ng bu zamonaviy ilmni o’rganishga sarflagan
-          vaqtingizga achinmaysiz!
-          <br />
-          <br />
-          Kompyuterda matn muharririni oching-da, avval alohida-alohida
-          harflarning yozilishini mashq qiling. Masalan, F va J harflarining
-          yozilishini. Avval to’rt marta FFFF deb yozib ko’ring, keyin to’rt
-          marta JJJJ harflarini yozing. Keyin aralash qilib, FFJJ FFJJ deb
-          yozasiz. Keyin esa, navbat bilan FJFJ FJFJ harflarini yozing. Bu
-          harflar yozilishini yaxshi o’zlashtirganingizdan keyin boshqa
-          harflarga o’tishingiz mumkin bo’ladi.
-        </p>
+        <h1>{t("foydali")}</h1>
+        <p>{helpText}</p>
       </div>
     </div>
   );
--- src/text.js
+++ src/text.js
@@ -12,3 +12,92 @@ NyaSA ma'lumotiga ko'ra Artemis missiyasi doirasida birinchi ayol Oyga parvoz qi
 Kelajakda odamlarning Oyga parvoz qilishi va qo'nishi ko'p jihatdan 2022 yildagi sinov parvozi muvaffaqiyatiga bog'liq Artemis I doirasidagi ilk parvoz 27 sentabrga rejalashtirilgan`;
 
 export const dummyTextEN = `Walking up to the towering walls of Great Zimbabwe was a humbling experience. The closer I got, the more they dwarfed me and yet, there was something inviting about the archaeological site. It didn't feel like an abandoned fortress or castle that one might see in Europe: Great Zimbabwe was a place where people lived and worked, a place where they came to worship and still do. It felt alive. Great Zimbabwe is the name of the extensive stone remains of an ancient city built between 1100 and 1450 CE near modern-day Masvingo, Zimbabwe. Believed to be the work of the Shona (who today make up the majority of Zimbabwe's population) and possibly other societies that were migrating back and forth across the area, the city was large and powerful, housing a population comparable to London at that time somewhere around 20,000 people during its peak. Great Zimbabwe was part of a sophisticated trade network (Arab, Indian and Chinese trade goods were all found at the site), and its architectural design was astounding: made of enormous, mortarless stone walls and towers, most of which are still standing. However, for close to a century, European colonisers of the late-19th and early-20th Centuries attributed the construction to outsiders and explorers, rather than to the Africans themselves. Indeed, the author of the first written European record of Great Zimbabwe seemed to be staggered by the very idea that it could have been built at all. Portuguese explorer Joao de Barros wrote in 1552 that, "There is masonry within and without, built of stones of a marvellous size, and there appears to be no mortar joining them. Visitors who come to Great Zimbabwe today can still explore three sections: the Hill Ruins (the oldest, with an acropolis believed to be a royal city); the Great Enclosure (surrounded by a large, high wall and containing an 11m conical tower); and the Valley Ruins (a collection of mud-brick houses where the majority of the ancient population lived). Cynthia Marangwanda, a writer, poet and heritage specialist who writes about Zimbabwean national identity, explained that "some people want to call it 'the Great Zimbabwe ruins', but I disagree with that: considering the kind of European meddling it has endured, it has stood up very well. In the Shona language, zimbabwe translates approximately to "stone house", and because of the site's size and scope, it became known as Great Zimbabwe. Moreover, it was not the only such "Zimbabwe": there are remains of approximately 200 smaller settlements or trading posts spread across the region, from the Kalahari Desert in Namibia to Mozambique. According to Munyaradzi Manyanga, a professor of archaeology and cultural heritage at Great Zimbabwe University, the position of Great Zimbabwe among these settlements has been widely debated. Some people have speculated that it was a capital city of a very large state, but to Manyanga, that seems unlikely. "Such a state would have been too large. One wouldn't have been able to manage that kind of extent and size. So most of the interpretations talk of these as having been influenced by Great Zimbabwe." He added that the Kingdom of Zimbabwe is considered to be made up of Great Zimbabwe and the smaller settlements located closer to it. One of the most remarkable features of the site is its walls. As Manyanga explained, "The style and scale of dry-stone walling that constitute Great Zimbabwe is unparalleled elsewhere in Africa and beyond." The walls of the Great Enclosure are 6m wide and 11m tall, and they run about 250m, making the enclosure the largest single structure in sub-Saharan Africa and the second largest on the continent by total area: only the pyramids in Egypt exceed them in size according to Manyanga. A decade later, in a speech to the Royal Geographic Society, British journalist Richard N Hall supported Bent's perspective after visiting the site himself. He talked about the artistic value of soapstone carvings that had been unearthed and the "marvellous cleverness" of a gold-mining operation that spanned hundreds of mines, before concluding that "it is quite a moral certainty that even the cruder methods of [these sciences'] application were imported from the Near East, and did not originate in South-East Africa." Instead, he and his colleagues held that Phoenicians, Arabians or Babylonians created the city. According to Manyanga, "They wanted to use [this explanation] as a moral justification for colonising Zimbabwe. If there was this long-lost civilisation in this part of the world, there was nothing wrong with colonialism because they were resuscitating this old kingdom.However, a few archaeologists of the time countered that the site was not nearly old enough to be from Biblical times. "The then-colonial government suppressed these views, and the official narrative in public media and museums was that Great Zimbabwe was of foreign origins," said Manyanga. This version of history was upheld through the 1960s and 1970s by the white-minority government of the colony. Only in 1980, when Zimbabwe achieved independence, could the new leaders finally affirm that the site was built by their own ancestors. During the 1960s, black nationalists had even settled on Zimbabwe as the name for the Washington Post`;
+
+export const helpTextUZ = ` Eng avvalo klaviaturangizga yaxshilab nazar soling. Biz ingliz (lotin)
+          harflari berilgan klaviaturaga qaramasdan yozishni o’rganamiz. Siz
+          ko’rib turgan lotin harflariga asoslangan tugmalarning joylashuvi
+          familiyasi Qwerty bo’lgan mutaxassis tomonidan joylashtirilgan. U
+          klaviaturaning birinchi qatoriga o’zining familiyasini «qwerty» qilib
+          joylashtirgan, shuning uchun hozir dunyoda eng ko’p ishlatiladigan bu
+          standart joylashuvli klaviatura «Qwerty klaviaturasi» deb ham ataladi.
+          
+          Har qanday klaviaturaning lotincha «F» va «J» harflari joylashgan
+          tugmalarda boshqalariga nisbatan ajralib turadigan, bir oz bo’rtib
+          chiqqan joyi bo’ladi. Buning sababi, ikki qo’lning ko’rsatkich
+          barmoqlari shu tugmalarda turishi kerak va matn teruvchi ko’rsatkich
+          barmoqlarini klaviaturaga qaramasdan, tugmachalarning bo’rtib turgan
+          joyini barmoq uchlari bilan his qilgan holda topib olishi kerak. Shu
+          tarzda, ikki qo’lning ko’rsatkich barmoqlari «F» va «J» harflariga
+          qo’yiladi, qolgan har ikki qo’ldagi uchtadan barmoq ketma-ket
+          joylashgan tugmachalar ustiga qo’yiladi. Klaviaturada qo’l mana bunday
+          holatda turishi kerak:
+         
+          Chap qo’l barmoqlari A S D F harflari yozilgan tugmalarda, o’ng qo’l
+          barmoqlari esa J K L ; tugmalari ustida turishi kerak. O’ng qo’lning
+          bosh barmog’i probel tugmasini bosich uchun ishlatilishi kerak.
+          
+          Quyidagi rasmda qo’llarning qaysi barmoqlari klaviaturadagi qaysi harf
+          tugmasini bosishi kerakligi ko’rsatilgan. E’tibor bering,
+          barmoqlarning ranglari ular bosishi kerak bo’lgan klaviatura tugmalari
+          ranglari bilan o’zaro mos:
+         
+          Endi klaviaturaga qaramasdan yozishni mash qila boshlar ekansiz,
+          Sizdan juda katta sabr talab etiladi. Qanchalik ko’p mashq qilsangiz,
+          shunchalik tez o’rganib olasiz. «Tez» deganda Siz klaviaturaga
+          qaramasdan yozishni bir necha soatda o’rganib olasiz, degan ma’no
+          tushunilmasligi kerak. Hatto bir necha kunda ham o’rganmasligingiz
+          mumkin. Bir necha hafta yoki hatto bir necha oylik shug’ullanishdan
+          keyin klaviaturaga qaramasdan, tezlik bilan matn terishni o’rganib
+          olasiz. Mashqda har qancha qiyin bo’lmasin, ishoning, maqsadingizga
+          erishganingizdan so’ng bu zamonaviy ilmni o’rganishga sarflagan
+          vaqtingizga achinmaysiz!
+          
+          Kompyuterda matn muharririni oching-da, avval alohida-alohida
+          harflarning yozilishini mashq qiling. Masalan, F va J harflarining
+          yozilishini. Avval to’rt marta FFFF deb yozib ko’ring, keyin to’rt
+          marta JJJJ harflarini yozing. Keyin aralash qilib, FFJJ FFJJ deb
+          yozasiz. Keyin esa, navbat bilan FJFJ FJFJ harflarini yozing. Bu
+          harflar yozilishini yaxshi o’zlashtirganingizdan keyin boshqa
+          harflarga o’tishingiz mumkin bo’ladi.`;
+
+export const helpTextEN = `First of all, take a good look at the keyboard. we english (latin)
+          We will study it by looking at the keyboard with letters. You
+          location of free keys to Latin letters you see
+          Q. U. about the specialist whose last name is Werty
+          typing his last name "qwerty" on the first line of the keyboard
+          placed, that's why it's the most thing in the world now
+          Standard layout keyboard also called "Qwerty keyboard.
+          Any keyboard has Latin letters "F" and "J".
+          the buttons stand out from the others, slightly convex
+          there will be a place where it comes out. The reason for this is the index of the two hands
+          fingers should stay on these keys and the typing indicator
+          his fingers are not looking at the keyboard, but the keys are protruding
+          he should find the place by feeling with his fingertips. That's it
+          the index fingers of both hands to the letters "F" and "J".
+          is placed, the remaining three fingers of each hand in a row
+          placed on the buttons. This is the hand on the keyboard
+          should be in position:
+          The fingers of the left hand are on the keys with the letters A S D F, the right hand
+          and the fingers are J K L ; should be on the buttons. Your right hand
+          the thumb should be used to press the probe button.
+          
+          In the picture below, which fingers of the hands represent which letter on the keyboard
+          it is indicated that he should press the button. Note that
+          the colors of the fingers are the keyboard keys they have to press
+          compatible with colors:
+          Now that you can start typing without looking at the keyboard,
+          You need a lot of patience. The more you practice, the
+          you will learn so quickly. When you say "quick" you to the keyboard
+          meaning that you can learn to write in a few hours without looking
+          should not be understood. You won't learn even in a few days
+          possible From a few weeks or even a few months of practice
+          then learning to type quickly without looking at the keyboard
+          you will get No matter how difficult it is in training, believe in your goal
+          Once you get it, it's modern.
+          Open a text editor on the computer, first individually
+          practice writing letters. For example, the letters F and J
+          to be written. First try typing FFFF four times, then four
+          write the letters JJJJ once. Then mix and say FFJJ FFJJ
+          you write Then write the letters FJFJ FJFJ in turn. This
+          after you have mastered the writing of letters well
+          you will be able to switch to letters.`;
