46bb926822f1c216a3c6201aa613e76001c6e05b
Boburbek2002 <rajabovboburbek02@gmail.com>
2022-11-17 15:54:32 +0500

    Change Navbar elements

--- src/Navbar/Nav.jsx
+++ src/Navbar/Nav.jsx
@@ -6,7 +6,7 @@ import sun_icon from "../assets/sun_icon.png";
 import moon_icon from "../assets/moon_icon.png";
 import { Switch } from "../components/Utils";
 
-function Nav({toggleLight, toggleDark}) {
+function Nav({ toggleLight, toggleDark }) {
   const [isOpen, setIsOpen] = useState(false);
   const { t, i18n } = useTranslation();
   const lang = localStorage.getItem("lang") || "uz";
@@ -17,14 +17,15 @@ function Nav({toggleLight, toggleDark}) {
 
   return (
     <div className="navbar">
-      <div className="navbar_name">
-        <h1>
-          fast <br />
-          write uz
-        </h1>
-      </div>
-      <div className={`navbar_list ${isOpen && "open"}`}>
-        {/* <ul>
+      <div className="leftElements">
+        <div className="navbar_name">
+          <h1>
+            fast <br />
+            write uz
+          </h1>
+        </div>
+        <div className={`navbar_list ${isOpen && "open"}`}>
+          {/* <ul>
           <li className="active">
             <Link to="/">{t("navbar1")}</Link>
           </li>
@@ -38,46 +39,50 @@ function Nav({toggleLight, toggleDark}) {
             <Link to="help">{t("navbar4")}</Link>
           </li>
         </ul> */}
-        <a href="">
-          <Link to="/">{t("navbar1")}</Link>
-        </a>
-        <a href="">
-          <Link to="history">{t("navbar2")}</Link>
-        </a>
-        <a href="">
-          <Link to="competition">{t("navbar3")}</Link>
-        </a>
-        <a href="">
-          <Link to="help">{t("navbar4")}</Link>
-        </a>
-        <a href=""></a>
+          <ul>
+            <li href="">
+              <Link to="/">{t("navbar1")}</Link>
+            </li>
+            <li href="">
+              <Link to="history">{t("navbar2")}</Link>
+            </li>
+            <li href="">
+              <Link to="competition">{t("navbar3")}</Link>
+            </li>
+            <li href="">
+              <Link to="help">{t("navbar4")}</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="select">
-        <select name="lang" id="lang" onChange={langHandler} value={lang}>
-          <option value="uz">UZ</option>
-          <option value="en">EN</option>
-        </select>
-      </div>
-      <div>
-        <Switch
-          img1={sun_icon}
-          img2={moon_icon}
-          toggleLight={toggleLight}
-          toggleDark={toggleDark}
-        />
-      </div>
-      <h2>
-        {localStorage.getItem("firstName")
-          ? localStorage.getItem("firstName") +
-            " " +
-            localStorage.getItem("lastName")
-          : ""}
-      </h2>
-      <div
-        className={`nav-toggle ${isOpen && "open"}`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="bar"></div>
+      <div className="rightElements">
+        <div className="view">
+          <Switch
+            img1={sun_icon}
+            img2={moon_icon}
+            toggleLight={toggleLight}
+            toggleDark={toggleDark}
+          />
+        </div>
+        <div className="select">
+          <select name="lang" id="lang" onChange={langHandler} value={lang}>
+            <option value="uz">UZ</option>
+            <option value="en">EN</option>
+          </select>
+        </div>
+        <h2>
+          {localStorage.getItem("firstName")
+            ? localStorage.getItem("firstName") +
+              " " +
+              localStorage.getItem("lastName")
+            : ""}
+        </h2>
+        <div
+          className={`nav-toggle ${isOpen && "open"}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className="bar"></div>
+        </div>
       </div>
     </div>
   );
--- src/components/Utils.jsx
+++ src/components/Utils.jsx
@@ -1,10 +1,20 @@
 export const Switch = ({ toggleLight, toggleDark, img1, img2 }) => {
   return (
     <div>
-      <button onClick={toggleLight}>
+      <button
+        onClick={(e) => {
+          toggleLight();
+          // e.currentTarget.classList.add("active");
+        }}
+      >
         <img src={img1} alt="Light" />
       </button>
-      <button onClick={toggleDark}>
+      <button
+        onClick={(e) => {
+          toggleDark();
+          // e.currentTarget.classList.add("active");
+        }}
+      >
         <img src={img2} alt="Dark" />
       </button>
     </div>
